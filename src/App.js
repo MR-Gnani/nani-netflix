754bc99cabc5fcf047145bc2ed2d0628e29ced76
nani kim <flsk9698@naver.com>
2024-04-07 01:19:15 +0900

    project set

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import AppLayout from './layout/AppLayout';
+import MainPage from './pages/MainPage/MainPage';
+import MovieDetailPage from './pages/MovieDetail/MovieDetailPage';
+import MoviePage from './pages/Movies/MoviePage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 
+// Main 페이지
+// Search 페이지
+// Detail 페이지
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<AppLayout/>}>
+        <Route index element={<MainPage/>}/>
+        <Route path='movies'>
+          <Route index element={<MoviePage/>}/>
+          <Route path=':id' element={<MovieDetailPage/>}/>
+        </Route>
+      </Route>
+
+      <Route path='*' element={<NotFoundPage/>}/>
+    </Routes>
   );
 }
 
