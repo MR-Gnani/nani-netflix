c0996bbb61d488311ceb25f0fc5456fa584a12ef
nani kim <flsk9698@naver.com>
2024-04-13 01:35:21 +0900

    finish

--- src/pages/MovieDetail/MovieDetailPage.jsx
+++ src/pages/MovieDetail/MovieDetailPage.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react'
 import './MovieDetailPage.style.css';
+import { Modal } from 'react-bootstrap';
+import YouTube from 'react-youtube';
 import Button from 'react-bootstrap/Button';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
@@ -9,6 +11,7 @@ import { useMovieByIdQuery } from '../../hooks/useImageById';
 import { useParams } from 'react-router-dom'
 import { useReviewByIdQuery } from '../../hooks/useReviewById';
 import SimilarMovieSlides from '../MainPage/components/Slides/SimilarMovieSlides';
+import { useMovieTrailerQuery } from '../../hooks/useMovieTrailer';
 
 const MovieDetailPage = () => {
   // 리뷰 란의 숨김/보임 상태를 관리하는 useState 훅
@@ -22,10 +25,19 @@ const MovieDetailPage = () => {
   // url에서 id값 가져오기
   let {id} = useParams();
  
+  const [show, setShow] = useState(false); 
+  const {data: video} = useMovieTrailerQuery(id); 
   const {data} = useMovieByIdQuery(id);
   const {data:review} = useReviewByIdQuery(id);
   const year = data?.release_date.split("-")[0];
-  console.log(review);
+  const opts = {
+    height: '100%',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+      modestbranding: 1, 
+    },
+  };
 
   return (
     <div className='detailSection'>
@@ -33,15 +45,18 @@ const MovieDetailPage = () => {
         <div className='textSection'>
           <h1>{data?.title}</h1>
           <div>
-            <Button className="m-1"variant="outline-light">ALL</Button>
+            <Button className="m-1"variant="warning">ALL</Button>
             {data?.genres.map((genre)=>(
               <Button className="m-1"variant="outline-light">{genre.name}</Button>
             ))}
             <Button className="m-1"variant="outline-light">{data?.runtime}m</Button>
             <Button className="m-1"variant="outline-light">{year}</Button>
+            <Button className="m-1"variant="danger">{data?.vote_average.toFixed(1)}</Button>
           </div>
           <div className='text-3'>
-            <Button className="m-1 shorts" variant="light">예고편 보기</Button>
+            <Button className="m-1 shorts" variant="light" onClick={() => setShow(true)}>
+              트레일러 보기
+            </Button>
             <div className='text-3-icon'>
               <FontAwesomeIcon icon={faHeart} className='m-1'/>
               <FontAwesomeIcon icon={faShareFromSquare} className='m-1'/>
@@ -70,6 +85,23 @@ const MovieDetailPage = () => {
       <div className='relatedMovie'>
           <SimilarMovieSlides movieId={id}/>
       </div>
+      <Modal
+        show={show}
+        centered={true}
+        onHide={() => setShow(false)}
+        dialogClassName='modal-90w'
+        contentClassName='modal-style'
+      >
+        <Modal.Header closeVariant='white' closeButton />
+        <Modal.Body>
+          <YouTube
+            videoId={video && video[0]?.key}
+            opts={opts}
+            style={{ height: '100%' }}
+            onReady={(event) => event.target.mute()}
+          />
+        </Modal.Body>
+      </Modal>
     </div>
   )
 }
--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSearchMovieQuery } from '../../hooks/useSearchMovies'
 import { useSearchParams } from 'react-router-dom'
 import {Alert} from "bootstrap";
@@ -6,55 +6,120 @@ import {Form} from "react-bootstrap";
 import './MoviePage.style.css';
 import MovieCards from '../../common/MovieCards/MovieCards';
 import ReactPaginate from 'react-paginate';
+import { useMovieGenreQuery } from '../../hooks/useMovieGenre';
+
+const CURRENT_YEAR = new Date().getFullYear();
 
 // 영화 검색 창.
-// navbar => popularMovie
-// keyword search => keyword와 관련된 영화
+// navbar => popularMovie,, keyword search => keyword와 관련된 영화
 const MoviePage = () => {
 
+  // state
+  const [data, setData] = useState(null);
+  const [sortValue, setSortValue] = useState('');
   const [rating, setRating] = useState(5);
   const [popularity, setPopularity] = useState(50);
-  const [year, setYear] = useState(2020);
-  const genres = [
-    "Action", "Romance", "Comedy", "War", "Science Fiction",
-    "Fantasy", "Crime", "Thriller", "Animation", "Horror"
-  ];
+  const [year, setYear] = useState([0, CURRENT_YEAR]);
   const[page, setPage] = useState(1);
-
-  // url에서 query가져오기
+  const [genreId, setGenreId] = useState([]);
+  // const genres = [
+  //   "Action", "Romance", "Comedy", "War", "Science Fiction",
+  //   "Fantasy", "Crime", "Thriller", "Animation", "Horror"
+  // ];
+  
+  // url query + API
   const [query, setQuery] = useSearchParams();
   const keyword = query.get("q")
-  const {data, isLoading, isError, error} = useSearchMovieQuery({keyword, page})
-  console.log("keyword", data);
+  const {data: movieList, isLoading, isError, error} = useSearchMovieQuery({keyword, page})
+  const {data: genreData} = useMovieGenreQuery();
+  console.log(data);
+
+  // function
+  // 왼쪽 slider 함수
+  const RangeSlider = ({ label, min, max, step, value, onChange }) => {
+    return (
+      <Form.Group>
+        <Form.Label>{label}</Form.Label>
+        <input
+          type="range"
+          min={min}
+          max={max}
+          step={step}
+          value={value}
+          onChange={onChange}
+          className="form-range"
+          style={{ cursor: 'pointer' }} // 커서 스타일 변경
+        />
+        <Form.Label>{value}</Form.Label>
+      </Form.Group>
+    );
+  };
+
+  // 영화 정렬 함수
+  const sortMovie = ()=>{
+    let sortedData;
+    switch(sortValue){
+      case 'popularity':
+        sortedData = [...data.results].sort(
+          (a, b) => b.popularity - a.popularity
+        );
+        setData({...data, results: sortedData});
+        return;
+      case 'vote':
+        sortedData = [...data.results].sort(
+          (a, b) => b.vote_average - a.vote_average
+        );
+        setData({...data, results: sortedData});
+        return;
+      case 'latest':
+        sortedData = [...data.results].sort(
+          (a,b)=>
+            Number(b.release_date.split('-').join('')) -
+            Number(a.release_date.split('-').join(''))
+        );
+        setData({...data, results: sortedData});
+        return;
+      default:
+        return;
+    }
+  };
+
+  // 장르선택 상태값 설정 함수
+  const handleGenreChange = (e,gId)=>{
+    if (e.target.checked) {
+      // 체크박스가 선택된 경우
+      setGenreId([...genreId, gId]);
+    } else {
+      // 체크박스가 해제된 경우
+      setGenreId(genreId.filter(id => id !== gId));
+    }
+  }
+  
+  // 페이지 핸들 함수
+  const handlePageClick = ({selected})=>{
+    setPage(selected+1);
+  };
+
+  // 정렬 상태값 설정 함수
+  const handleSortChange = (e)=>{
+    setSortValue(e.target.value);
+  }
+
+  useEffect(()=>{
+    if(sortValue !== ''){
+      sortMovie();
+    } else if(movieList) {
+      setData(movieList);
+    }
+  },[sortValue, movieList]);
+
+  // useEffect 뒤에 와야함.
   if(isLoading){
     return <h1> Loading ... </h1>
     }
     if(isError){
         return <Alert variant="danger">{error.message}</Alert>;
     }
-
-    const RangeSlider = ({ label, min, max, step, value, onChange }) => {
-      return (
-        <Form.Group>
-          <Form.Label>{label}</Form.Label>
-          <input
-            type="range"
-            min={min}
-            max={max}
-            step={step}
-            value={value}
-            onChange={onChange}
-            className="form-range"
-            style={{ cursor: 'pointer' }} // 커서 스타일 변경
-          />
-          <Form.Label>{value}</Form.Label>
-        </Form.Group>
-      );
-    };
-  
-  const handlePageClick = ({selected})=>{
-    setPage(selected+1);
-  };
 
   return (
     <div className='SearchPage'>
@@ -63,17 +128,19 @@ const MoviePage = () => {
           <div className='sortDiv'>
             <h3>정렬</h3>
             <div className='labelSection'>
-            <label> <input className="margin" type="radio" name="gender" value="male"/>인기순</label>
-            <label> <input className="margin" type="radio" name="gender" value="female"/>평점순</label>
-            <label> <input className="margin" type="radio" name="gender" value="other" />최신순</label>
+            <label> <input className="margin" type="radio" checked={sortValue === 'popularity'} onChange={handleSortChange} name="popularity" value="popularity"/>인기순</label>
+            <label> <input className="margin" type="radio" checked={sortValue === 'vote'} onChange={handleSortChange} name="vote" value="vote"/>평점순</label>
+            <label> <input className="margin" type="radio" checked={sortValue === 'latest'} onChange={handleSortChange} name="latest" value="latest" />최신순</label>
             </div>
           </div>
           <div className='genreDiv'>
             <h3>장르</h3>
-            {genres.map((genre, index) => (
+            {genreData.map((item, index) => (
               <div  className='' key={index}>
-                <input type="checkbox" id={`genre-${index}`} name={`genre-${index}`} />
-                <label htmlFor={`genre-${index}`}>{genre}</label>
+                <input type="checkbox" id={`genre-${index}`} name={`genre-${index}`}
+                checked={genreId.includes(item.id)}
+                onChange={(e) => handleGenreChange(e, item.id)} />
+                <label htmlFor={`genre-${index}`}>{item.name}</label>
               </div>
             ))}
             <div className='none'> . </div>
@@ -89,7 +156,7 @@ const MoviePage = () => {
           <RangeSlider 
             label="Popularity" 
             min={0} 
-            max={100} 
+            max={5000} 
             step={1} 
             value={popularity} 
             onChange={(e) => setPopularity(e.target.value)} 
@@ -104,7 +171,9 @@ const MoviePage = () => {
           />
         </div>
         <div className='resultSection'>
-          {data?.results.map((movie, index)=><MovieCards key={index} movie={movie}/>)}
+          {data?.results
+          .filter((item)=>genreId.every((i)=>item.genre_ids.includes(i)))
+          ?.map((movie, index)=><MovieCards key={index} movie={movie}/>)}
         </div>
       </div>
       <div className='pageSection'>
